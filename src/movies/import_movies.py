0ab98df6c6cab264e65806b0b48fc3629af6d465
“natios” <natios100@gmail.com>
2023-04-21 17:50:07 +0200

    importy danych

---
+++ src/movies/import_movies.py
@@ -0,0 +1,32 @@
+from asyncio import run, sleep
+
+import asyncpg
+from dotenv import load_dotenv
+from os import getenv
+
+import pandas as pd
+
+import json
+
+from db_class import DbService
+
+from model import Movie
+from src.movies.analysis_tools import get_movies
+
+
+async def main():
+    db = DbService()
+    await db.initialize()  # tu łączymy się z bazą danych
+    movies = get_movies('data/tmdb_5000_movies.csv')
+    print(f'all movies: {len(movies)}')
+
+    for i, movie in enumerate(movies):
+        await db.upsert_actor(movie)
+        if i % 100 == 0:
+            print(f'import in {i / len(movies) * 100:.1f}% done')
+
+    await sleep(1)
+
+
+if __name__ == '__main__':
+    run(main())
