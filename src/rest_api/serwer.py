f0a29778534387232d1f62d328a21bbe673b8958
Piotr Marecki <pmarecki@gmail.com>
2023-09-23 10:35:33 +0200

    feat: add db-related class with async initialization

--- src/rest_api/serwer.py
+++ src/rest_api/serwer.py
@@ -1,13 +1,17 @@
 from asyncio import sleep
 from dataclasses import dataclass
+from os import getenv
 from random import randint
 from uuid import uuid4, UUID
 
 import aiohttp_cors
+import asyncpg
 from aiohttp import web
 from aiohttp.abc import Application
+from dotenv import load_dotenv
 
 routes = web.RouteTableDef()
+app = web.Application()
 
 """
 https://docs.aiohttp.org/en/stable/web_quickstart.html#
@@ -33,13 +37,6 @@ async def hello(request):
     return web.json_response({'comment': 'Welcome!'})
 
 
-@routes.get('/render')
-async def render(request):
-    # ciężka operacja -- wykorzystująca run_in_executor
-    consumers.append(randint(1, 10))
-    return web.json_response({'comment': f'Welcome {consumers}!'})
-
-
 @routes.get('/square')
 async def hello(request):
     # odpalanie: http://0.0.0.0:4000/square?x=12
@@ -48,14 +45,16 @@ async def hello(request):
     xx = x ** 2
     return web.json_response({'result': xx})
 
+
 @routes.get('/user')
 async def hello(request):
     # odpalanie: http://0.0.0.0:4000/user?name=Radek
     name: str = request.rel_url.query['name']
 
-    user = User(id=randint(1,100), name=name)
-    return web.json_response(user.__dict__) # można zwrócić list[dict] etc..
+    # sposób na wykorzystanie metod serwisu bazodanowego
+    user = await app['db'].get_user_by_name(name=name)
 
+    return web.json_response(user.__dict__)  # można zwrócić list[dict] etc..
 
 
 @dataclass
@@ -64,8 +63,35 @@ class User:
     name: str
 
 
-if __name__ == '__main__':
-    app = web.Application()
-    app.add_routes(routes)
+load_dotenv()
+URL = getenv('DATABASE_URL')
+SCHEMA = getenv('SCHEMA')
 
-    web.run_app(app, port=4000)
+
+class DbService:
+    # to jest prototyp klasy z dostępem do bazy danych -- todo rozwinąć go jak dotychczas
+
+    async def initialize(self):
+        # self.pool = await asyncpg.create_pool(URL, timeout=30, command_timeout=5, min_size=15, max_size=20,
+        #                                       server_settings={'search_path': SCHEMA})
+
+        print('connected!')
+
+    async def foo(self):
+        await sleep(0.5)
+        return User(id=123, name='Xiaomi')
+
+    async def get_user_by_name(self, name: str):
+        return User(id=123, name=name)
+
+
+async def app_factory():
+    db = DbService()
+    await db.initialize()
+    app.add_routes(routes)
+    app['db'] = db
+    return app
+
+
+if __name__ == '__main__':
+    web.run_app(app_factory(), port=4000)
